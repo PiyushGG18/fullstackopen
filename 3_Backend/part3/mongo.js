078ad814b8296e127da72bd672afc73b57905c69
PiyushGG18 <gawadepiyush007@gmail.com>
2026-08-14 09:19:23 +0000

    Explore Databases
    The Commit focuses on exporation of Mongo DB and
    marks the completion of task 3.12

---
+++ 3_Backend/part3/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3){
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@database.orepcc0.mongodb.net/noteApp?appName=database`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+})
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
