4477084ad4aa6fb69471f9a4fefbd3dc72e65fc0
PiyushGG18 <gawadepiyush007@gmail.com>
2026-08-20 03:55:08 +0000

    uibuild

--- 3_Backend/part3/index.js
+++ 3_Backend/part3/index.js
@@ -6,8 +6,9 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+
 
 
 let notes = [
@@ -28,13 +29,6 @@ let notes = [
     }
 ]
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
-
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
     console.log('Path:', request.path)
@@ -44,6 +38,26 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
+
+const errorHandler = (eror, request, response, next) => {
+    console.log(error.message)
+
+    if(error.name === "CastError"){
+        return response.status(400).send({error: 'malformed id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -55,10 +69,15 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if(note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -80,10 +99,31 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+
+    Note.findById(request.params.id)
+        .then(note => {
+            if(!note){
+                return response.status(404).end()
+            }
+
+            note.content = content
+            note.important = important
+
+            return note.save().then((updatedNote) => {
+                response.json(updatedNote)
+            })
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
